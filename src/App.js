52dd4f8c65385b02e63ab75d09b13ecc6ff73577
netcse <imran.cse.kuet@gmail.com>
2025-02-02 14:37:34 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-=======
 import Form from "./components/Form";
 
 const App = () => {
   return <Form />;
 };
->>>>>>> 7ad25161b99ae028389ea0a55cc686214c44f7fb
 
 export default App;

588821b39f2f62e88937feb5149af164d32fb2b4
Amran <imran.cse.kuet@gmail.com>
2025-03-03 20:21:42 +0600

    updated form

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
-import FlavorForm from './FlavorForm';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <FlavorForm />
+    <App />
   </React.StrictMode>
 );
 
